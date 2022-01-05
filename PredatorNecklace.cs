e2ea98f139420aa592fd0ba75b93aad1c8570cc7
Honda <90362468+HondaCivicMoment@users.noreply.github.com>
2022-01-05 08:34:56 -0500

    Nearly finished the crafting recipe

---
+++ PredatorNecklace.cs
@@ -0,0 +1,34 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BlossomBeloved.Items.Equipment.Accessories
+{
+    public class PredatorNecklace : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Predator Necklace");
+            Tooltip.SetDefault("Extra armor penetration");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 42;
+            item.height = 46;
+            item.value = 10000;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SharkToothNecklace);
+            //recipe.AddIngredient(ItemID.SharkToothNecklace);
+            recipe.AddTile(TileID.TinkerersWorkbench);
+            recipe.SetResult(this);
+            recipe.AddRecipe;
+        }
+        public override bool CanEquipAccessory(Player player, int slot)
+        {
+            return base.CanEquipAccessory(player, slot);
+        }
+    }
+}
