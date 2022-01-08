5a48c868d47f9621aee9c43e599421b888ea9f5d
Honda <90362468+HondaCivicMoment@users.noreply.github.com>
2022-01-08 16:19:10 -0500

    Add files via upload

---
+++ Npcs/Friendly/BarkSummon.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace BlossomBeloved.Npcs.Friendly.BarkSummon
+{
+    public class BarkSummon : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Endemic Member");
+        }
+        public override void SetDefaults()
+        {
+            //npc.CloneDefaults=NPCID.
+            npc.width = 24;
+            npc.height = 34;
+            npc.friendly = true;
+        }
+    }
+}
